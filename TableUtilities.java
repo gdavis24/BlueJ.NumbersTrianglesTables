54a391087ebba2bcd82f01eeb40046f8655a2d51
Gregory Davis <gdavis@zipcodes-MacBook-Pro.local>
2020-02-05 18:16:13 -0500

    uploaded completed lab

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,15 +1,60 @@
- 
+
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+      String result = "";
+      for (int i = 1; i <= 5; i ++){
+        for (int j = 1; j <= 5; j ++){
+          if (i*j < 10){
+          result += "  " + i*j + " |";
+        }
+          else if (i*j < 100){
+            result += " " + i*j + " |";
+          }
+          else {
+            result += i*j + " |";
+          }
+        }
+        result += "\n";
+      }
+      return result;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+      String result = "";
+      for (int i = 1; i <= 10; i ++){
+        for (int j = 1; j <= 10; j ++){
+          if (i*j < 10){
+          result += "  " + i*j + " |";
+        }
+          else if (i*j < 100){
+            result += " " + i*j + " |";
+          }
+          else {
+            result += i*j + " |";
+          }
+        }
+        result += "\n";
+      }
+      return result;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+      String result = "";
+      for (int i = 1; i <= tableSize; i ++){
+        for (int j = 1; j <= tableSize; j ++){
+          if (i*j < 10){
+          result += "  " + i*j + " |";
+        }
+          else if (i*j < 100){
+            result += " " + i*j + " |";
+          }
+          else {
+            result += i*j + " |";
+          }
+        }
+        result += "\n";
+      }
+      return result;
     }
 }
